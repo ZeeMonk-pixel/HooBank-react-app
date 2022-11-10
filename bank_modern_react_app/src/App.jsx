afd4ce3f44925c186cdf8c51115790dc959a3545
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2022-11-10 13:31:29 +0100

    edits

---
+++ bank_modern_react_app/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './styles';
+
+import {Navbar, Billing, Card, Business, Clients, CTA, Stats, Footer, Testimonials, HeroSection} from './components';
+
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <HeroSection />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <Card /> 
+        <Testimonials /> 
+        <Clients /> 
+        <CTA /> 
+        <Footer /> 
+      </div>
+    </div>
+  </div>
+);
+
+export default App
