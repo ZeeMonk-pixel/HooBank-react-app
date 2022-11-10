233f549728122928697db26df5a4928905f19326
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2022-11-10 14:25:31 +0100

    edits

--- bank_modern_react_app/src/App.jsx
+++ bank_modern_react_app/src/App.jsx
@@ -6,30 +6,33 @@ import {Navbar, Billing, Card, Business, Clients, CTA, Stats, Footer, Testimonia
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
+    <div className="max-w-[1500px] mx-auto">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
-    </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <HeroSection />
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <HeroSection />
+        </div>
       </div>
-    </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        <Billing />
-        <Card /> 
-        <Testimonials /> 
-        <Clients /> 
-        <CTA /> 
-        <Footer /> 
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <Card /> 
+          <Testimonials /> 
+          <Clients /> 
+          <CTA /> 
+          <Footer /> 
+        </div>
       </div>
     </div>
+    
   </div>
 );
 
